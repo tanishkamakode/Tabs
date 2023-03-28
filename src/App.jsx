dd1368c2fc206b50970f065e9ffb03b8d2c1b638
Tanishka <makodetanishka@gmail.com>
2023-03-28 20:08:22 +0530

    Set first job

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,34 @@
-const url = 'https://course-api.com/react-tabs-project';
+const url = "https://course-api.com/react-tabs-project";
+import { useState, useEffect } from "react";
+import JobInfo from "./JobInfo";
 
 const App = () => {
-  return <h2>Tabs Starter</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+
+  const fetchJobs = async () => {
+    const repsonse = await fetch(url);
+    const newJobs = await repsonse.json();
+    setJobs(newJobs);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className="jobs-center">
+        <div className="loading"></div>
+      </section>
+    );
+  }
+
+  return (
+    <section className="jobs-center">
+      <JobInfo jobs={jobs} />
+    </section>
+  );
 };
 export default App;
---
+++ src/Duties.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Duties = ({ duties }) => {
+  return <div></div>;
+};
+
+export default Duties;
